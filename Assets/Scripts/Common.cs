d390fb7978f28e96d07fc43337326d84d8e531d4
noble-shake <coppgabliw@gmail.com>
2024-03-19 21:52:23 +0900

    2024-03-19 UnityClass 03

---
+++ Assets/Scripts/Common.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Layers { 
+    Ground
+}
+
+public enum Tags { 
+    Player,
+    Item
+}
+
+public static class Tool {
+    public static string GetLayer(Layers _value) {
+        return _value.ToString();
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Animator anim;
-    Rigidbody2D rigid;
-    Vector3 moveDir;
+    [SerializeField] Animator anim;
+    [SerializeField] Rigidbody2D rigid;
+    [SerializeField] Vector3 moveDir;
+    [SerializeField] BoxCollider2D boxColl;
 
-    float moveSpeed = 5f;
-    float jumpForce = 5f;
-    float verticalVelocity;
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float verticalVelocity;
 
-    bool isJump;
-    bool isGround;
+    [SerializeField] bool isJump;
+    [SerializeField] bool isGround;
+
+    [SerializeField] float rayDistance = 1f;
+    [SerializeField] Color rayColor;
+    [SerializeField] bool showRay = false;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        boxColl = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (showRay == true) {
+            Gizmos.color = rayColor;
+            Gizmos.DrawLine(transform.position, transform.position - new Vector3(0, rayDistance));
+
+            // Gizmos color = Color.blue;
+            // Gizmos.DrawLine(transform.position, transform.position - new Vector3(0, rayDistance));
+        }
+
     }
 
     // Update is called once per frame
@@ -28,6 +46,38 @@ public class Player : MonoBehaviour
     {
         checkGrounded();
         moving();
+        doJump();
+        checkGravity();
+    }
+
+    private void doJump()
+    {
+        if (isGround == false) return;
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            isJump = true;
+        }
+    }
+
+    private void checkGravity()
+    {
+        if (isGround == false) {
+            verticalVelocity = Physics2D.gravity.y * Time.deltaTime;
+            if (verticalVelocity < -10.0f)
+            {
+                verticalVelocity = -10.0f;
+            }
+            else {
+                if (isJump == true) {
+                    isJump = false;
+                    verticalVelocity = jumpForce;
+                }
+                if (verticalVelocity < 0) {
+                    verticalVelocity = 0f;
+                }
+            }
+        }
+        rigid.velocity = new Vector2(rigid.velocity.x, verticalVelocity);
     }
 
     private void checkGrounded()
@@ -36,21 +86,23 @@ public class Player : MonoBehaviour
 
         if (verticalVelocity > 0f) return;
 
-        RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector3.down, 1, LayerMask.GetMask("Ground")); //this int exactly different general int value. maybe checksome Layer gab.
-        if (ray) { 
-            
+        // RaycastHit2D ray = Physics2D.Raycast(transform.position, Vector3.down, 1, LayerMask.GetMask(Tool.GetLayer(Layers.Ground))); //this int exactly different general int value. maybe checksome Layer gab.
+        RaycastHit2D ray = Physics2D.BoxCast(boxColl.bounds.center, boxColl.bounds.size, 0f, Vector3.down, rayDistance, LayerMask.GetMask(Tool.GetLayer(Layers.Ground)));  //bounds -> bounding box concept
+
+        if (ray) {
+            isGround = true;    
         }
     }
 
     private void moving() {
        moveDir.x = Input.GetAxis("Horizontal") * moveSpeed;
         moveDir.y = rigid.velocity.y;
-        rigid.velocity = moveDir * jumpForce;
+        rigid.velocity = moveDir;
         anim.SetBool("Walk", moveDir.x != 0.0f);
 
         if (moveDir.x != 0.0f) { //go to right then x=1, scale x = -1, if left, x= -1, scale x = 1
             Vector3 locScale = transform.localScale;
-            locScale.x = moveDir.x * -1;
+            locScale.x = -1 * Input.GetAxis("Horizontal");
             transform.localScale = locScale;
         }
     }
